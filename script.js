f22aa0545c5b5dc75711284cb62837e2674a2444
Vladislav Tikhonovich <vt@rmz.by>
2023-04-29 21:45:02 +0300

    feat: Made animation for button. Started task for write symbol in input.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
         ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del"],
         ["Caps Lock","a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"],
         ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "↑", "Shift"],
-        ["Ctrl", "Win", "Alt","Space", "Alt", "Ctrl","←","↓","→"]
+        ["Ctrl L", "Win", "Alt","Space", "Alt", "Ctrl R","←","↓","→"]
     ];
 
     const keysRu = [
@@ -20,7 +20,7 @@ window.addEventListener("load", function() {
         ["Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "Del"],
         ["Caps Lock","ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter"],
         ["Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "↑", "Shift"],
-        ["Ctrl", "Win", "Alt","Space", "Alt", "Ctrl","←","↓","→"]
+        ["Ctrl L", "Win", "Alt","Space", "Alt", "Ctrl R","←","↓","→"]
     ];
 
     keysEn.forEach(row => {
@@ -30,6 +30,7 @@ window.addEventListener("load", function() {
         row.forEach(key => {
             const keyEl = document.createElement("button");
             keyEl.classList.add("key");
+            keyEl.classList.add(key.replace(/ .*/,''));
             keyEl.textContent = key;
             rowEl.appendChild(keyEl);
             
@@ -61,8 +62,8 @@ window.addEventListener("load", function() {
     document.addEventListener('keydown', function(e) {
         const key = [...keyboard.querySelectorAll('.key')].find(k => {
             const isCapsLock = e.code === 'CapsLock' && k.textContent === 'Caps Lock';
-            const isCtrl = e.code === 'ControlLeft' && k.textContent === 'Ctrl' ||
-                e.code === 'ControlRight' && k.textContent === 'Ctrl';
+            const isCtrl = e.code === 'ControlLeft' && k.textContent === 'Ctrl L' ||
+                e.code === 'ControlRight' && k.textContent === 'Ctrl R';
             const isWin = e.code === 'MetaLeft' && k.textContent === 'Win';
             const isSpace = e.code === 'Space' && k.textContent === 'Space';
             const isDel = e.code === 'Delete' && k.textContent === 'Del';
@@ -82,8 +83,8 @@ window.addEventListener("load", function() {
     document.addEventListener('keyup', function(e) {
         const key = [...keyboard.querySelectorAll('.key')].find(k => {
             const isCapsLock = e.code === 'CapsLock' && k.textContent === 'Caps Lock';
-            const isCtrl = e.code === 'ControlLeft' && k.textContent === 'Ctrl' ||
-                e.code === 'ControlRight' && k.textContent === 'Ctrl';
+            const isCtrl = e.code === 'ControlLeft' && k.textContent === 'Ctrl L' ||
+                e.code === 'ControlRight' && k.textContent === 'Ctrl R';
             const isWin = e.code === 'MetaLeft' && k.textContent === 'Win';
             const isSpace = e.code === 'Space' && k.textContent === 'Space';
             const isDel = e.code === 'Delete' && k.textContent === 'Del';
@@ -96,6 +97,25 @@ window.addEventListener("load", function() {
         if (key) {
           key.classList.remove('pressed');
         }
+      });
+
+    const keys = keyboard.querySelectorAll('.key');
+      console.log(keys)
+      keys.forEach(key => {
+        key.addEventListener('mousedown', function(e) {
+          if (key.textContent === 'Ctrl L' || key.textContent === 'Ctrl R' || key.textContent === 'Shift' || key.textContent === 'Alt') {
+            return; 
+          }
+          key.classList.add('pressed');
+          inputField.value += key.textContent;
+        });
+      
+        key.addEventListener('mouseup', function(e) {
+          if (key.textContent === 'Ctrl' || key.textContent === 'Ctrl R' || key.textContent === 'Shift' || key.textContent === 'Alt') {
+            return;
+          }
+          key.classList.remove('pressed');
+        });
       });
 
     document.body.appendChild(keyboard);
